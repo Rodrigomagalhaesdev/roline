1dc363bc28525bc1a703d3f4d115e42f528cdd26
Rodrigo M. <rodrigo.magalhaesdev93@gmail.com>
2024-06-11 22:59:27 -0300

    Subindo a aplicação da minha vida.

---
+++ src/index.js
@@ -0,0 +1,107 @@
+// Hide effect
+const myObserver = new IntersectionObserver((entries) => {
+  console.log(entries);
+
+  for (let entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  }
+});
+
+const elements = document.querySelectorAll(".hidden");
+
+elements.forEach((element) => myObserver.observe(element));
+
+// Machine of words
+// document.addEventListener("DOMContentLoaded", () => {
+//   const typewriterElement = document.querySelector(".text-2");
+//   const text = typewriterElement.textContent;
+//   typewriterElement.textContent = "";
+//   let i = 0;
+
+//   function type() {
+//     if (i < text.length) {
+//       typewriterElement.textContent += text.charAt(i);
+//       i++;
+//       setTimeout(type, 90);
+//     }
+//   }
+
+//   type();
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const typewriterElements = document.querySelectorAll(".text-2");
+
+//   typewriterElements.forEach(typewriterElement => {
+//     const text = typewriterElement.textContent;
+//     typewriterElement.textContent = "";
+//     let i = 0;
+
+//     function type() {
+//       if (i < text.length) {
+//         typewriterElement.textContent += text.charAt(i);
+//         i++;
+//         setTimeout(type, 90);
+//       }
+//     }
+
+//     type();
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const typewriterElements = document.querySelectorAll(".text-2");
+
+  function typeWriter(element, text) {
+    return new Promise((resolve) => {
+      let i = 0;
+      element.textContent = "";
+      element.style.visibility = "visible"; // Mostrar o elemento quando começar a escrever
+
+      function type() {
+        if (i < text.length) {
+          element.textContent += text.charAt(i);
+          i++;
+          setTimeout(type, 90);
+        } else {
+          resolve();
+        }
+      }
+
+      type();
+    });
+  }
+
+  async function startTyping() {
+    for (const element of typewriterElements) {
+      const text = element.textContent;
+      element.style.visibility = "hidden"; // Esconder o elemento inicialmente
+    }
+
+    for (const element of typewriterElements) {
+      const text = element.textContent;
+      await typeWriter(element, text);
+    }
+  }
+
+  startTyping();
+});
+
+
+
+var swiper = new Swiper(".swiper", {
+  cssMode:true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
